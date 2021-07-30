485c4edac292286407aff52053f7783be61e0c71
alifiarahmah <alifiarahmah03@gmail.com>
2021-07-30 21:57:40 +0700

    restructured lib

---
+++ lib/screens/landing.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'welcome.dart';
+
+class Landing extends StatelessWidget {
+  // Edufun logo
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: FlatButton(
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Welcome()));
+          },
+          child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ), // TODO: cari cara buat bener2 ketengahin
+                  Image.asset("images/logo.png",
+                      height: MediaQuery.of(context).size.height * 0.6),
+                  Text(
+                    "Ketuk untuk mulai",
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  )
+                ],
+              )),
+        ));
+  }
+}
