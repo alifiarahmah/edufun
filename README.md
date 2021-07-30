579ac5d2f49e5009af094ad1a32114fd4e0d1863
alifiarahmah <alifiarahmah03@gmail.com>
2021-07-30 19:15:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# edufun
+
+Team name: Mantan TPB<br>
+Team members:<br>
+Feodora Putri Humaira<br> 
+Amila Safira Arafani<br>
+Nadhira Aalia Syifaa<br>
+Alifia Rahmah<br>
+
+## Product Description
+
+---
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Landing(),
+    );
+  }
+}
+
+class Landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Edufun")
+        ]
+      ),
+    );
+  }
+}
